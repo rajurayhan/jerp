5fb6b1ceebe9a1b2055519c704c86d8ae8010c86
Raju at LHG <raju@lhgraphics.com>
2020-11-24 22:20:14 +0600

    API Doc Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('apidoc.*', function ($view) {
             $view->with([
-                'baseUrl'   => 'http://203.188.246.138:8885/',
+                'baseUrl'   => 'http://203.188.246.138:8885',
                 'token'     => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTBhNmMzMWYyN2FkYjU3ODFkNDlkYmFhMTZiZmY4ZGU1N2I2YTIyMWQ3NzA3MjExMjZhMjU1MWUxODIzMmI1NzMzYTJjNjM4NTA0MTQ0MDIiLCJpYXQiOjE2MDAzMzU3MDIsIm5iZiI6MTYwMDMzNTcwMiwiZXhwIjoxNjMxODcxNzAyLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.dNEln530pvlXF8vyQeTz7ApdbfIa3aINvYLMcd6fUKbIHSVF40t9jmib1L_jI4EfjF7tszOGHhlyICds0mZmIA'
             ]);
         });
